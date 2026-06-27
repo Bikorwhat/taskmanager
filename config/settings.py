acb02a332665b9b0b63e1abc0eeb0ed4a6a5ec5b
Bik <nepbik12@gmail.com>
2026-06-27 19:50:06 +0545

    added CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -148,3 +148,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://projectmanger01.up.railway.app'
+]
